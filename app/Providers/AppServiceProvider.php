e34408905254adcf0d1b03552602189bfe454167
Germán Cascales <GermanCascales@users.noreply.github.com>
2022-07-12 22:05:21 +0200

    Config alias tipo modelo en polimorfismo reportes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot() {
+        Relation::enforceMorphMap([
+            'post' => 'App\Models\Post',
+            'comment' => 'App\Models\Comment'
+        ]);
     }
 }
